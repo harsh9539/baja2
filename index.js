88a22bcf808b13f13076e338cbb428869fe822ef
Harsh Goyal <76143659+harsh9539@users.noreply.github.com>
2024-03-18 06:12:39 +0000

    test

--- index.js
+++ index.js
@@ -11,12 +11,30 @@ app.get("/",(req,res)=>{
 app.post("/bfhl",(req,res)=>{
 
     const reqBody = req.body;
-    console.log(reqBody);
+    if(!reqBody.data){
+        res.json({
+            "is_success": false,
+            "user_id": "john_doe_17091999", "email" : "john@xyz.com",
+            "roll_number":"ABCD123",
+    })
+    }
 
-    const oddNumbers = reqBody.data.map((num)=>{
-        if(typeof num === "number" && num % 2 != 0){
-            return num;
+    const oddNumbers = [];
+    const evenNumber = [];
+    const alphabets = [];
+    reqBody.data.forEach((ele)=>{
+        if(typeof ele === 'number'){
+            if(ele % 2 == 0){
+                evenNumber.push(ele);
+            }
+            else{
+                evenNumber.push(ele);
+            }
         }
+        else{
+            let c = ele.toUpperCase();
+            alphabets.push(alphabets);
+        };
     })
 
     // res.send(reqBody.data);
@@ -26,8 +44,8 @@ app.post("/bfhl",(req,res)=>{
             "user_id": "john_doe_17091999", "email" : "john@xyz.com",
             "roll_number":"ABCD123",
             "odd_numbers": oddNumbers,
-            "even_numbers": ["334","4"],
-            "alphabets": ["A","R"]
+            "even_numbers": evenNumber,
+            "alphabets": alphabets
     })
     
 })
